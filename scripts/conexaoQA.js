660c81db4e92a3b13aca1456ac9a161f67136a4c
Isaque Ribeiro Fererira <38390651+isaquerr25@users.noreply.github.com>
2023-11-14 19:34:25 -0400

    ccc

--- scripts/conexaoQA.js
+++ scripts/conexaoQA.js
@@ -12,20 +12,10 @@ export const options = {
         // ramp-down from 5 to 0 VUs in 5s
         { duration: '5s', target: 0 }
     ],
-    thresholds: {
-        // throws error if more than 90% of the requests takes more than 2 seconds to be completed
-        http_req_duration: [
-            {
-                threshold: 'p(90) < 15000',
-                abortOnFail: true,
-                delayAbortEval: 100
-            }
-        ]
-    }
 }
 
-export default function() {
-    const response = http.get('http://conexaoqa.herokuapp.com/api/profile')
+export default function () {
+    const response = http.get('http://10.109.54.13/80')
     check(response, { "status is 200": (r) => r.status === 200 })
     sleep(.300)
 }
